12bd8225f0a5013a7640fbc1cac15feb4b0ecb05
Lisa <llvvss@icloud.com>
2023-04-23 17:17:13 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def commonCharacters(strings):
+    # Write your code here.
+    return []
+
+if __name__ == '__main__':
+    print(commonCharacters(["abc", "bcd", "cbaccd"]))
+    print(commonCharacters(["a", "d", "cbaccd"]))
+    print(commonCharacters(["abcde", "bcd", "b"]))
+    if "apple" in ["apple", "banana"]: print ("yes")
